a85a815719cccef00c429b06fdec12efda7ba0ec
Stefan Kupresak <stefan_vg@hotmail.com>
2018-07-15 18:39:20 +0200

    Add mockup for the tasks preview

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import "./tasks.css";
 class Tasks extends React.Component {
   render() {
-
     return (
-      <h1>Hello! From Tasks :)</h1>
+      <div className="Tasks">
+        <div className="TasksPanel">
+          <h3 className="TasksPanel__title">
+            Completed tasks
+          </h3>
+          <p className="TasksPanel__completed">
+            0
+          </p>
+        </div>
+      </div>
     )
   }
 }
