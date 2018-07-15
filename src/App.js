945f8e4249a6ec128f27e8e09484d572a58090c1
Stefan Kupresak <stefan_vg@hotmail.com>
2018-07-15 23:12:52 +0200

    add some css and proper navagation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 
 import routes from './components/router-ext/routes';
 import Navigation from './components/layout/Navigation';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import PropsRoute from './components/router-ext/PropsRoute';
 
 import Tasks from './components/Tasks';
 import Mentors from './components/Mentors';
+
+/* Font Awesome */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTasks } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faTasks);
 
 class App extends Component {
   render() {
@@ -14,7 +20,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <Navigation />
-          <div class="content">
+          <div className="content">
             <PropsRoute exact path={routes.tasks} component={Tasks} />
             <PropsRoute exact path={routes.mentors} component={Mentors} />
           </div>
--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import routes from '../router-ext/routes';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import './navigation.css';
 
 const Navigation = () => (
-  <aside class='Navigation-navbar'>
+  <aside className='Navigation-navbar'>
     <ul>
       <li>
-        <Link to={routes.tasks}>Tasks</Link>
+        <Link to={routes.tasks} className="d-flex align-items-center">
+          <FontAwesomeIcon icon="tasks" />
+          <span class="m-l-p33">Tasks</span>
+        </Link>
       </li>
       <li>
         <Link to={routes.mentors}>Mentors</Link>
