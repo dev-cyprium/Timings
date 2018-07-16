eb1d09ab39d1e31d54863647ca55e88fa86b45b9
Stefan Kupresak <stefan_vg@hotmail.com>
2018-07-16 15:01:01 +0200

    change label for h1

--- src/task/TaskList.js
+++ src/task/TaskList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TaskList = (props) => (
   <div className="TaskList">
     <div className="TaskList__title">
-      <h1>List of Tasks</h1>
+      <h1>List of available tasks</h1>
     </div>
   </div>
 );
