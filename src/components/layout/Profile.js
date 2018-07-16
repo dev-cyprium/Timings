4b3be7c7d8a8e085db97187df973e3f7347eb107
Stefan Kupresak <stefan_vg@hotmail.com>
2018-07-16 10:24:29 +0200

    Tweeks to side navbar

--- src/components/layout/Profile.js
+++ src/components/layout/Profile.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 import ProfileImage from '../../images/profile.jpg';
 
+import './profile.css';
+
 const Profile = () => (
-  <img src={ProfileImage} />
+  <div className='Profile'>
+    <div className='Profile__image'>
+      <img src={ProfileImage} />
+    </div>
+    <div className="Profile__name">
+      <h1>Stefan Kuprešak</h1>
+      <span>No task in progress</span>
+    </div>
+  </div>
 );
 
 export default Profile;
