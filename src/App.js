a681bdf58f1dc6f3db7bfab1867471ef6ee1d764
Stefan Kupresak <stefan_vg@hotmail.com>
2018-07-16 15:18:37 +0200

    Add helpfull comments in the App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+/* Dependencies */
 import routes from './router-ext/routes';
 import Navigation from './layout/Navigation';
 import { BrowserRouter as Router } from 'react-router-dom';
 import PropsRoute from './router-ext/PropsRoute';
 
+/* Main Application Views */
 import TaskView from './task/TaskView';
 import Mentors from './mentor/Mentors';
 
@@ -12,6 +14,7 @@ import Mentors from './mentor/Mentors';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faTasks, faUsers } from '@fortawesome/free-solid-svg-icons'
 
+// TODO: move to a seperate class that adds to the library
 library.add(faTasks);
 library.add(faUsers);
 
