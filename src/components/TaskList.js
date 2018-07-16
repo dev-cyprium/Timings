457403eebe5e1e3d99d77121e272f79ea740ebdd
Stefan Kupresak <stefan_vg@hotmail.com>
2018-07-16 13:41:46 +0200

    Add tasklist component

---
+++ src/components/TaskList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TaskList = (props) => (
+  <h1>List tasks</h1>
+);
+
+export default TaskList;
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,18 +1,37 @@
 import React from 'react';
 
 import TaskPanelInfo from './TaskPanel';
+import TaskList from './TaskList';
 
 import "./tasks.css";
+
+const tasks = [
+  {
+    name: "Task #1",
+    author: "Stefan Kuprešak",
+    langs: ["JavaScript", "HTML", "CSS"],
+    libs: ["React"],
+    difficulty: {
+      rate: 3,
+      max_rate: 5
+    }
+  }
+]
 
 // TODO: rename TaskPAnel component to taskpanelinfo
 class Tasks extends React.Component {
   render() {
     return (
-      <div className="Tasks">
-        <TaskPanelInfo label="Completed Tasks" val="15" color="neon-blue" />
-        <TaskPanelInfo label="Pending Tasks"   val="4"  color="neon-blue" />
-        <TaskPanelInfo label="Average Score"   val="78.2%" color="neon-green" />
-        <TaskPanelInfo label="Taks per day"    val="0.3" color="neon-blue" />
+      <div>
+        <div className="Tasks">
+          <TaskPanelInfo label="Completed Tasks" val="15"    color="neon-blue" />
+          <TaskPanelInfo label="Pending Tasks"   val="4"     color="neon-blue" />
+          <TaskPanelInfo label="Average Score"   val="78.2%" color="neon-green" />
+          <TaskPanelInfo label="Taks per day"    val="0.3"   color="neon-blue" />
+        </div>
+        <div className="Tasks">
+          <TaskList tasks={tasks} />
+        </div>
       </div>
     )
   }
