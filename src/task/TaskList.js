e12ba2d4d84d735742c7b7aaa5e3b0bd6e2333ab
Olja <olja.ivkovic97@gmail.com>
2018-07-16 21:40:08 +0200

    Tweeks to CSS

--- src/task/TaskList.js
+++ src/task/TaskList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const TaskList = (props) => (
+import Task from './Task';
+
+const TaskList = () => (
   <div className="TaskList">
     <div className="TaskList__title">
       <h1>List of available tasks</h1>
-
-      <ul>
-        <li>Bla bla bla bla</li>
-      </ul>
     </div>
+      <Task />
+      <Task />
   </div>
 );
 
