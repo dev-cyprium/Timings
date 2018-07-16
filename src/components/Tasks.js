e0cad71f8e98e5d8380681d9852662924d0f1f2c
Stefan Kupresak <stefan_vg@hotmail.com>
2018-07-16 13:22:50 +0200

    Rename component to taskpanelinfo

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
-import TaskPanel from './TaskPanel';
+import TaskPanelInfo from './TaskPanel';
 
 import "./tasks.css";
+
+// TODO: rename TaskPAnel component to taskpanelinfo
 class Tasks extends React.Component {
   render() {
     return (
       <div className="Tasks">
-        <TaskPanel label="Completed Tasks" val="15" color="neon-blue" />
-        <TaskPanel label="Pending Tasks"   val="4"  color="neon-blue" />
-        <TaskPanel label="Average Score"   val="78.2%" color="neon-green" />
-        <TaskPanel label="Taks per day"    val="0.3" color="neon-blue" />
+        <TaskPanelInfo label="Completed Tasks" val="15" color="neon-blue" />
+        <TaskPanelInfo label="Pending Tasks"   val="4"  color="neon-blue" />
+        <TaskPanelInfo label="Average Score"   val="78.2%" color="neon-green" />
+        <TaskPanelInfo label="Taks per day"    val="0.3" color="neon-blue" />
       </div>
     )
   }
